5eb1499cd97317cc8b1c070f983d3eba7d191ac6
Amberlie Porter <whenamesattacks@gmail.com>
2022-07-05 22:14:24 -0700

    incomplete attempt 1

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
-// Assignment code here
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
+function generatePassword() {
+  let output = "";
+  
+  const passwordTxt = document.getElementById("password");
+  const length = document.getElementById("length");
+  const incNumbers = document.getElementById("numbers");
+  const incSymbols = document.getElementById("symbols");
+
+  const generatePassword = (length, characters) => {
+    let password = "";
+       for (let i = 0; i < length; i++) {
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+  return password;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +33,14 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", () => {
+  let characters = alpha;
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordTxt.value = generatePassword(length.value, characters);
+});
+writePassword()
+}
